c9dfc545659a8144364fc85b6421a0187903ce95
ndsl9219 <ndsl971@proton.me>
2023-01-25 22:09:25 +0300

    task completed

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix == undefined) {
+    return [];
+  }
+
+  let total = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    if (i % 2 === 1) {
+      total = total.concat(matrix[i].reverse());
+    } else 
+      total = total.concat(matrix[i]);
+  }
+  
+  return total;
 }
